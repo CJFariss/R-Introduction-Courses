2483a45dc79df91366975a10fc04914b5558ed71
Christopher Fariss <cjf0006@gmail.com>
2024-11-04 13:41:14 -0500

    Update myRplot.R

--- easyRplots/myRplot.R
+++ easyRplots/myRplot.R
@@ -2,7 +2,7 @@
 ##########################################################################
 ## Student name: your_first_name_here your_last_name_here
 ## COURSE NAME: Human Rights
-## University of Michigan, Winter 2023, Fall 2022
+## University of Michigan, Fall 2024, Fall 2023, Winter 2023, Fall 2022
 ##########################################################################
 
 ## hint01: you only need to know getwd(), read.csv(), and plot() or barplot() to complete the R plot assignment for class
@@ -42,7 +42,7 @@ barplot(data$total)
 ## use the code from the end of the easyRplots.R file to modify your dataset
 par(mar=c(8,4,2,2))
 barplot(data$total, 
-        col="blue", 
+        col=c("orange"), 
         main="title of the graph", 
         ylab="number of stops", 
         xlab="year", 
